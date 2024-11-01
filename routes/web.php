b5d8eecf74a2bbaed412d9ede5defe62312e37d5
RizzCode10 <rakarizi9@gmail.com>
2024-11-01 22:08:55 +0700

    laravel online shop backend

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('user', UserController::class);
+    //category
+    Route::resource('category', \App\Http\Controllers\CategoryController::class);
+    //product
+    Route::resource('product', \App\Http\Controllers\ProductController::class);
+});
